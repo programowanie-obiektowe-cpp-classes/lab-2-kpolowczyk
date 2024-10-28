0de8ae649863c8523bfa1f89db85190687de5179
Kamil Polowczyk <kamil.polowczyk.stud@pw.edu.pl>
2024-10-28 08:44:26 +0100

    z2_v0

--- ResourceManager.hpp
+++ ResourceManager.hpp
@@ -4,5 +4,30 @@
 
 class ResourceManager
 {
-    // Twoja implementacja tutaj
+public:
+    // Konstruktor domyślny: Alokuje zasób w pamięci
+    ResourceManager()
+    {
+        resource_ptr = new Resource();
+    }
+
+    // Destruktor: zwalnia pamięć zasobu
+    ~ResourceManager()
+    {
+        delete resource_ptr;
+    }
+
+    // Funkcja dostępu do zasobu bezpośrednio z pola tablicy `tab`
+    double getResourceValue() const
+    {
+        return (*resource_ptr).tab[Resource::get_index];
+    }
+
+    double get()
+    {
+        return (*resource_ptr).get();
+    }
+
+    private:
+    Resource* resource_ptr; // Surowy wskaźnik do zasobu
 };
